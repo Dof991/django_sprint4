49a29de791a15025dad942f5c524b73a18d5b3d0
Dof991 <148769502+Dof991@users.noreply.github.com>
2025-05-09 21:49:39 +0300

    werwerwer
    erw

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,18 +1,23 @@
+# pages/views.py
+# Представления для статических страниц и ошибок
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView
 
 from .models import Page
 
 
-def csrf_failure(request, reason=""):
-    return render(request, 'pages/403csrf.html', status=403)
+def csrf_failure(request, reason="", exception=None):
+    """Обработчик CSRF-ошибок."""
+    return render(request, 'pages/403csrf.html', {'reason': reason}, status=403)
 
 
 def page_not_found(request, exception):
+    """Обработчик 404 ошибок."""
     return render(request, 'pages/404.html', status=404)
 
 
 def server_error(request):
+    """Обработчик 500 ошибок."""
     return render(request, 'pages/500.html', status=500)
 
 
