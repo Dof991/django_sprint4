6d7ef1f39d8cd3baa09095004f555a302f0c38f2
Dof991 <148769502+Dof991@users.noreply.github.com>
2025-05-09 18:23:41 +0300

    etret

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from blog.views import RegistrationView
 
 handler403 = 'pages.views.csrf_failure'
 handler404 = 'pages.views.page_not_found'
@@ -12,4 +13,6 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
     path('auth/', include('django.contrib.auth.urls')),
+    path('auth/registration/',
+         RegistrationView.as_view(), name='registration'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
