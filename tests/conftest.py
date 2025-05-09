dc38e5316732e916a26cc9dd144675dae9afb2a9
Dof991 <148769502+Dof991@users.noreply.github.com>
2025-05-10 02:40:45 +0300

    qweq

--- tests/conftest.py
+++ tests/conftest.py
@@ -35,6 +35,7 @@ UrlRepr = NamedTuple("UrlRepr", [("url", str), ("repr", str)])
 TitledUrlRepr = TypeVar("TitledUrlRepr", bound=Tuple[UrlRepr, str])
 
 
+
 @pytest.fixture(autouse=True)
 def enable_debug_false():
     with override_settings(DEBUG=False):
