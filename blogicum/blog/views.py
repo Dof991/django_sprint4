7eb26690360b86c12b1a9913106b27931ff61afe
Dof991 <148769502+Dof991@users.noreply.github.com>
2025-05-09 20:21:55 +0300

    werwer

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -66,23 +66,23 @@ def category_posts(request, category_slug):
 
 @login_required
 def profile(request, username):
-    profile = get_object_or_404(User, username=username)  # Используем profile вместо profile_user
+    profile = get_object_or_404(User, username=username)
     post_list = profile.posts.select_related('category', 'location').annotate(
         comment_count=Count('comments')
     )
-    
+
     if request.user != profile:
         post_list = post_list.filter(
             is_published=True,
             category__is_published=True,
             pub_date__lte=timezone.now()
         )
-    
+
     paginator = Paginator(post_list.order_by('-pub_date'), 10)
     return render(request, 'blog/profile.html', {
-        'profile': profile,  # Используем profile как в шаблоне
+        'profile': profile,
         'page_obj': paginator.get_page(request.GET.get('page')),
-        'user': request.user  # Добавляем user для проверки auth
+        'user': request.user
     })
 
 
@@ -98,11 +98,12 @@ class ProfileEditView(LoginRequiredMixin, UpdateView):
     template_name = 'blog/user.html'
 
     def get_object(self, queryset=None):
-        return self.request.user  # Редактируем только текущего пользователя
+        return self.request.user
 
     def get_success_url(self):
-        return reverse('blog:profile',
-                       kwargs={'username': self.request.user.username}
+        return reverse(
+            'blog:profile',
+            kwargs={'username': self.request.user.username}
         )
 
 
